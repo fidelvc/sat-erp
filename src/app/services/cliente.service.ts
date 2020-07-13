10d252f29a566aafe74bf8ae1840a4c4f83f4b0b
fidelvc <fidel@qtech.es>
2020-07-14 00:48:35 +0200

    feat: URL agregado en cliente.service desde enviroments

--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { Cliente } from '../models/cliente';
+
+const URL = environment.URL;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ClienteService {
-
-  constructor() { }
+  constructor() {}
 }
